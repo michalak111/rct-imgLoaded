c4907f3cca3f5f6d88d123b10fac903af84f6db7
michalak111 <michalak111@gmail.com>
2018-03-29 21:21:13 +0200

    Create repository bundler

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.scss'
+import ImgLoaded from './ImgLoaded'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <ImgLoaded />
+      </div>
+    )
+  }
+}
---
+++ src/ImgLoaded.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class ImgLoaded extends Component {
+  state = {
+    lol: 'lol'
+  }
+  render() {
+    return <div>image</div>
+  }
+}
